723180e9542b7dfa08a1bfefafe2cf80ad39fc59
KyleDuncan <duncankylet@gmail.com>
2016-01-12 20:12:21 -0500

    Basic structure for the app

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+var journalText = document.getElementById(".journal-entry").innerHTML;
+var jounralStorage = Window.localStorage;
+/*
+
+***| Testing for localStorage capability |***
+function storageAvailable(type) {
+	try {
+		var storage = window[type],
+			x = '__storage_test__';
+		storage.setItem(x, x);
+		storage.removeItem(x);
+		return true;
+	}
+	catch(e) {
+		return false;
+	}
+}
+
+
+if (storageAvailable('localStorage')) {
+	// Yippee! We can use localStorage awesomeness
+}
+else {
+	// Too bad, no localStorage for us
+}
+*/
+
+var newEntry = function() {
+	journalText = '';
+	
+	
+};
+
+var listEntries = function() {
+	
+	
+};
+
+var saveEntry = function() {
+	
+};
+
+var processData = function() {
+	
+};
